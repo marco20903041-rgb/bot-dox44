2500fb966ab024aafd3cdb0a8e3f50b9e4fc38f7
MARCO <marco20903041@gmail.com>
2026-05-13 09:20:33 -0500

    solucionado

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from  flask import Flask
 
 
 # Tokens
-BOT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1hcmNvMjA5MDMwNDFAZ21haWwuY29tIn0.czP-vmTPvH_F8KSBSSAKNLRUmL10aJEY3sV_as3etWU""
+BOT_TOKEN = "8714125008:AAGnNawfd0A_mVoZStJsrASu1bNylaYvJOg"
 API_TOKEN = os.environ.get("API_PERU_TOKEN")
  # Tu token de CODART.
 
